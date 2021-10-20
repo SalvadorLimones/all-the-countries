b05ea7d83b8ac2a6cd454b1aa0ad0a1dbd48d9fb
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2021-10-20 18:23:32 +0200

    change tabulation

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
 async function loadEvent() {
-  const rootElement = document.getElementById("root");
+    const rootElement = document.getElementById("root");
 
-  const res = await fetch("https://restcountries.com/v3.1/all");
-  const countries = await res.json();
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const countries = await res.json();
 
-  console.log(countries);
+    console.log(countries);
 
-  for (const country of countries) {
-    const countryHTML =`
-    <section class="independent">
-        <img src="${country.flags.png}" alt="flag">
-        <h1>${country.name.official}</h1>
-        <h2>${country.capital}</h2>
-        <h4>Languages: ${countryLanguage()}</h4>
-        <h3>${landLocked()}</h3>
-    </section>
-    `;
+    for (const country of countries) {
+        const countryHTML =`
+        <section class="independent">
+            <img src="${country.flags.png}" alt="flag">
+            <h1>${country.name.official}</h1>
+            <h2>${country.capital}</h2>
+            <h4>Languages: ${countryLanguage()}</h4>
+            <h3>${landLocked()}</h3>
+        </section>
+        `;
       
-    function countryLanguage(){
-        console.log(`${country.name.common}:`);
-        var myLanguages=[];
-        for (const i in country.languages){
-            console.log(country.languages[i]);
-            myLanguages.push(country.languages[i]);
+        function countryLanguage(){
+            console.log(`${country.name.common}:`);
+            var myLanguages=[];
+            for (const i in country.languages){
+                console.log(country.languages[i]);
+                myLanguages.push(country.languages[i]);
+            }
+            return(myLanguages);   
         }
-        return(myLanguages);   
+
+        function landLocked(){
+            if(country.landlocked){
+                return("I've never seen the sea!")
+            } else{
+                return("");
+            }
+        } 
+      
+        rootElement.insertAdjacentHTML("beforeend", countryHTML);  
     }
-
-    function landLocked(){
-        if(country.landlocked){
-            return("I've never seen the sea!")
-        } else{
-            return("");
-        }
-    } 
-      
-    rootElement.insertAdjacentHTML("beforeend", countryHTML);  
-}
     
- /* const peruO = peru[0];
-
-
-  for (const i in peruO.languages){
-    console.log(peruO.languages[i]);
-  }
-  console.log(peruO.landlocked);
-  
-
-  console.log(rootElement);
-  
-  rootElement.insertAdjacentHTML("beforeend", peruHTML); */
 }
-
 
 window.addEventListener("load", loadEvent);
